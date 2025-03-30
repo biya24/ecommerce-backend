4164ad084ae6381b114fdd90af85746abc1ecb4b
biya24 <biyabinoy@gmail.com>
2025-03-30 18:50:56 +0530

    Fix order verification5

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post('/', protect, placeOrder);
 router.get('/', protect, getOrders);
 router.get("/admin", protect, adminOnly, getAllOrdersAdmin);
+// ✅ Fetch logged-in user's orders
+router.get("/my-orders", protect, getUserOrders);
 // ✅ Get Single Order
 router.get("/:id", protect, getOrderById);
 // ✅ Admin Delete Order
@@ -14,8 +16,7 @@ router.delete("/:id/admin", protect, adminOnly, deleteOrderByAdmin);
 
 router.put('/:orderId/status', protect, updateOrderStatus); //route for updating order status
 
-// ✅ Fetch logged-in user's orders
-router.get("/my-orders", protect, getUserOrders);
+
 
 
 module.exports = router;
